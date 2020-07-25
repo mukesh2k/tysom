bc5e4dd70b749e74a23373208a4bd296ecd354f1
Mukesh <mukesh192k@gmail.com>
2020-07-25 18:12:45 +0530

    starter

---
+++ cpp/c.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int rever(long a)
+{
+    if (a==1)return 1;
+     return a*rever(a-1);
+}
+main()
+{
+    cout<<"jfsad";
+    cout<<rever(4);
+    for(int i=1;i>0;i++)cout<<"hellooo";
+}
